27178e5af604225629b6da7ab8a8cab680f72107
DJEBBI Alaeddine <62078514+alaeddinedjebbi@users.noreply.github.com>
2021-07-29 03:24:25 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Alaeddine from './userList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Liste des utilisateurs :</h1>
+      <Alaeddine />         
+    </div>
+  );
+}
+
+export default App;
